3587d7923abd481d3ef33bffea653d74079e7768
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2024-12-18 01:42:05 +0530

    Changed gopath, setup few main files, added mux and signal handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Abhinash-kml/Golang-React-Social-media/pkg/db"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	db.Connect()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, os.Kill)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigs
+
+	// Shutdown()
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
-module socialmedia
+module github.com/Abhinash-kml/Golang-React-Social-media
 
 go 1.23.1
-
-require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-)
