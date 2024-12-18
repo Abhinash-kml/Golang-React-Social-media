991574365ecd7de23af67b0974d91e6c808bed4b
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2024-12-19 00:18:38 +0530

    .env and prototypes

--- pkg/models/media.go
+++ pkg/models/media.go
@@ -1,4 +1,27 @@
 package models
 
 type Media struct {
+	ID     int    `json:"id"`
+	PostID int    `json:"post_id"`
+	Url    string `json:"url"`
+}
+
+func NewMedia() {
+
+}
+
+func InsertMedia() {
+
+}
+
+func UpdateMedia() {
+
+}
+
+func DeleteMedia() {
+
+}
+
+func GetMedia() {
+
 }
--- pkg/models/post.go
+++ pkg/models/post.go
@@ -1,4 +1,26 @@
 package models
 
+import "github.com/google/uuid"
+
 type Post struct {
+	ID      int       `json:"id"`
+	UserID  uuid.UUID `json:"user_id"`
+	Content string    `json:"content"`
+	Hashtag string    `json:"hashtag"`
+}
+
+func InsertPost() {
+
+}
+
+func UpdatePost() {
+
+}
+
+func DeletePost() {
+
+}
+
+func GetPost() {
+
 }
