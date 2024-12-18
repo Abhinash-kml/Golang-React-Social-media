e859a839e5123fc7d302396242f8af10b2d1d88d
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2024-12-18 21:29:03 +0530

    Implemented db functions

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.23.1
 
 require (
 	github.com/google/uuid v1.6.0
-	go.uber.org/zap v1.27.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
-
-require go.uber.org/multierr v1.10.0 // indirect
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,5 +1,47 @@
 package db
 
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var connection *sql.DB
+
 func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Couldn't load .env file")
+	}
+
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", DATABASE_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if db == nil {
+		log.Fatal("Returned db is still invalid")
+	}
+
+	connection = db
+
+	CreateTables()
+}
+
+func Disconnect() {
+	if connection == nil {
+		log.Fatal("Trying to close a databse connection which is already closed or invalid")
+	}
+
+	connection.Close()
+	connection = nil
+}
+
+func CreateTables() {
 
 }
