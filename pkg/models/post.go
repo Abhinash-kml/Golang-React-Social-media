5d5108cb6ec64a61f044cd3a1f4378932e411408
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2024-12-19 23:57:47 +0530

    Added some more functions

--- pkg/models/post.go
+++ pkg/models/post.go
@@ -25,26 +25,55 @@ func NewPost(uuid uuid.UUID, content, hashtag string) *Post {
 func InsertPost(uuId uuid.UUID, content, hashtag string) (bool, error) {
 	row := db.Connection.QueryRow("INSERT INTO posts(userid, content, hashtag) VALUES($1, $2, $3) returning userid;", uuId, content, hashtag)
 	var returnedId uuid.UUID
-	if err := row.Scan(returnedId); err != nil {
+	if err := row.Scan(&returnedId); err != nil {
 		fmt.Println("Error occured in Inserting post. Error: ", err)
 		return false, err
 	}
 
 	if returnedId != uuId {
-		return false, errors.New("the returned uuid from insertion is not same as inserted uuid in InsertPost()")
+		return false, errors.New("the returned uuid from insertion is not same as supplied uuid in InsertPost()")
 	}
 
 	return true, nil
 }
 
-func UpdatePost() {
+func UpdatePost(uuId uuid.UUID, newContent, hashtag string) (bool, error) {
+	row := db.Connection.QueryRow("UPDATE posts SET content = $1, hastag = $2 WHERE userid = $3 RETURNING userid;", newContent, hashtag, uuId)
+	var returnedId uuid.UUID
+	if err := row.Scan(&returnedId); err != nil {
+		fmt.Println("Error occured in updating post. Error: ", err)
+		return false, err
+	}
 
+	if returnedId != uuId {
+		return false, errors.New("the returned uuid from update in not same as supplied uuid in UpdatePost()")
+	}
+
+	return true, nil
 }
 
-func DeletePost() {
+func DeletePostWithId(uuId uuid.UUID) (bool, error) {
+	row := db.Connection.QueryRow("DELETE FROM posts WHERE userid = $1 returning userid;", uuId)
+	var returnedId uuid.UUID
+	if err := row.Scan(&returnedId); err != nil {
+		fmt.Println("Error occured in deleting post. Error: ", err)
+		return false, err
+	}
 
+	if returnedId != uuId {
+		return false, errors.New("the returned uuid from delete is not same as supplied uuid in DeletePostWithId()")
+	}
+
+	return true, nil
 }
 
-func GetPost() {
+func GetPost(uuId uuid.UUID) *Post {
+	row := db.Connection.QueryRow("SELECT * FROM posts WHERE userid = $1;", uuId)
+	post := &Post{}
+	if err := row.Scan(post.UserID, post.Content, post.Hashtag); err != nil {
+		fmt.Println("Error occured in getting post. Error: ", err)
+		return nil
+	}
 
+	return post
 }
