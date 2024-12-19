67848151379d8fec94d349402afbcd098344a422
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2024-12-20 04:14:43 +0530

    Some refactoring and more query functions

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/Abhinash-kml/Golang-React-Social-media/pkg/db"
 	"github.com/google/uuid"
@@ -9,7 +11,7 @@ import (
 )
 
 type User struct {
-	Id          uuid.UUID `json:"id"`
+	Id          uuid.UUID `json:"uuid"`
 	Fullname    string    `json:"name"`
 	Email       string    `json:"email"`
 	Password    string    `json:"password"`
@@ -40,7 +42,10 @@ func GetUserWithID(logger *zap.Logger, id uuid.UUID) (*User, error) {
 	row := db.Connection.QueryRow("SELECT * FROM users WHERE id = $1;", id)
 	if err := row.Scan(user.Id, user.Fullname, user.Email, user.Password, user.Dob, user.Created_at, user.Modified_at, user.Lastlogin); err != nil {
 		logger.Error("Error scanning row",
-			zap.String("function", "GetUserWithId"))
+			zap.String("function", "GetUserWithId"),
+			zap.String("Error", err.Error()))
+
+		return nil, err
 	}
 
 	return user, nil
@@ -70,21 +75,73 @@ func GetUserWith(logger *zap.Logger, with string, condition string, value string
 	row := db.Connection.QueryRow(query)
 	if err := row.Scan(user.Id, user.Fullname, user.Email, user.Password, user.Dob, user.Created_at, user.Modified_at, user.Lastlogin); err != nil {
 		logger.Error("Error scanning row",
-			zap.String("funcion", "GetUserWith"))
+			zap.String("Funcion", "GetUserWith"),
+			zap.String("Error", err.Error()))
+
 		return nil, err
 	}
 
 	return user, nil
 }
 
-func UpdateUser() {
-	// logger, _ := zap.NewProduction()
+func UpdateUserWithId(logger *zap.Logger, userid uuid.UUID, name, email, country, state string) (bool, error) {
+	row := db.Connection.QueryRow("UPDATE users SET name = $1, email = $2, country = $3, state = $4 WHERE userid = $5 RETURNING userid;", name, email, country, state, userid)
+	var returnedId uuid.UUID
+	if err := row.Scan(&returnedId); err != nil {
+		logger.Error("Error scanning row",
+			zap.String("Function", "UpdateUserWithId"),
+			zap.String("Error", err.Error()))
+
+		return false, err
+	}
+
+	if returnedId != userid {
+		return false, errors.New("returned userid is not same as supplied userid in UpdateUserWithId()")
+	}
+
+	return true, nil
 }
 
-func DeleteUser() {
+func DeleteUserWithId(logger *zap.Logger, userid uuid.UUID) (bool, error) {
+	row := db.Connection.QueryRow("DELETE FROM users WHERE userid = $1 RETURNING userid;", userid)
+	var returnedId uuid.UUID
+	if err := row.Scan(&returnedId); err != nil {
+		logger.Error("Error scanning row",
+			zap.String("Function", "DeleteUserWithId"),
+			zap.String("Error", err.Error()))
 
+		return false, err
+	}
+
+	if returnedId != userid {
+		return false, errors.New("returned userid is not same as supplied userid in DeleteUserWithId()")
+	}
+
+	return true, nil
 }
 
-func InsertUser() {
+func InsertUser(logger *zap.Logger, fullname, email, password, dob, country, state, city string) (bool, error) {
+	uuId, _ := uuid.NewRandom()
+	createdAt := time.Now().String()
+	modifiedAt := time.Now().String()
+	lastLogin := time.Now().String()
 
+	row := db.Connection.QueryRow(`INSERT INTO users(uuid, fullname, email, password, dob, country, state, city, created_at, modified_at, last_login)
+								VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING user_id`,
+		uuId, fullname, email, password, dob, country, state, city, createdAt, modifiedAt, lastLogin)
+
+	var returnedId uuid.UUID
+	if err := row.Scan(&returnedId); err != nil {
+		logger.Error("Error scanning row",
+			zap.String("Function", "InsertUser"),
+			zap.String("Error", err.Error()))
+
+		return false, err
+	}
+
+	if returnedId != uuId {
+		return false, errors.New("the returned userid is not same as the supplied userid")
+	}
+
+	return true, nil
 }
